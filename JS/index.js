7aff5543a84fb3ff91a55e76e60536acdcee4644
ljac86 <laura.young023@gmail.com>
2024-08-15 18:30:25 -0400

    Javascript animations

--- JS/index.js
+++ JS/index.js
@@ -25,3 +25,41 @@ $(document).ready(function() {
         
     );  
 });
+
+$(document).ready(function() {
+    $('li').hover(
+        function() {
+            $(this).css('text-decoration', 'underline');
+            $(this).css('cursor', 'pointer');
+            $(this).css('font-weight', 'bold');
+
+        }, 
+        function() {
+            $(this).css('text-decoration', 'none');
+            $(this).css('cursor', 'default');
+            $(this).css('font-weight', 'normal');
+        }
+    );
+});
+
+$(document).ready(function() {
+    $('button').hover(
+        function() {
+            $(this).css('box-shadow', '0 8px 12px rgba(0, 0, 0, 0.4)'); // Add shadow on hover
+        },
+        function() {
+            $(this).css('box-shadow', '0 2px 4px rgba(0, 0, 0, 0.2)'); // Remove shadow when not hovering
+        }
+    );
+});
+
+$(document).ready(function() {
+    $('h3').hover(
+        function() {
+            $(this).css('cursor', 'pointer');
+        }, 
+        function() {
+            $(this).css('cursor', 'default');
+        }
+    );
+});
