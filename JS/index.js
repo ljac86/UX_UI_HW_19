f467fa06ec13582321cb8c2318d424ada0daab21
ljac86 <laura.young023@gmail.com>
2024-08-15 14:16:51 -0400

    Jquery animations to My work section

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.travelEAZE').hover(
+        function() {
+            // Mouse enters
+            $(this).addClass('enlarged');
+        },
+        function() {
+            // Mouse leaves
+            $(this).removeClass('enlarged');
+        }
+        
+    );  
+});
+
+$(document).ready(function() {
+    $('.sanctuary').hover(
+        function() {
+            // Mouse enters
+            $(this).addClass('enlarged');
+        },
+        function() {
+            // Mouse leaves
+            $(this).removeClass('enlarged');
+        }
+        
+    );  
+});
